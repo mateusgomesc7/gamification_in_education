1706b070c821dbfec419a29b27af0ea3bcbbb28e
Mateus Gomes <mateusgomes@ieee.org>
2020-07-26 09:16:57 -0300

    test

--- frontend/components/login/LoginForm.vue
+++ frontend/components/login/LoginForm.vue
@@ -5,7 +5,7 @@
       dark
       flat
     >
-      <v-toolbar-title>Login form</v-toolbar-title>
+      <v-toolbar-title>Login</v-toolbar-title>
       <v-spacer />
       <v-tooltip bottom>
         <template v-slot:activator="{ on }">

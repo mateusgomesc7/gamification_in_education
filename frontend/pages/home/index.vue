d0d3ae4a9b57bebe05c89386afd8f687ed86d6bc
Laura Macaiba <laurinhaaalvs@gmail.com>
2020-08-31 17:20:28 -0300

    pagina home (Card-Duvida)

--- frontend/pages/home/index.vue
+++ frontend/pages/home/index.vue
@@ -1,5 +1,78 @@
 <template>
-  <h1>Página da Home</h1>
+  <v-container class="grey lighten-5">
+    <v-row no-gutters>
+      <v-col
+        class="pa-2"
+        md="8"
+      >
+        <v-row>
+          <v-col>
+            <v-card
+              rounded="xl"
+              elevation="4"
+              color="grey"
+              class="pa-2 text-center "
+            >
+              <v-card-text>
+                <h2 class="display-1 my-5">
+                  Qual sua Duvida?
+                </h2>
+                <v-divider class="my-4" />
+                <v-btn color="success" class="my-5">
+                  Faça uma Pergunta
+                </v-btn>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-card
+              class="pa-2"
+              outlined
+              tile
+            >
+              .col-6 .col-md-4
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-card
+              class="pa-2"
+              outlined
+              tile
+            >
+              .col-6 .col-md-4
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-card
+              class="pa-2"
+              outlined
+              tile
+            >
+              .col-6 .col-md-4
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col
+        class="pa-2"
+        md="4"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          .col-6 .col-md-4
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
